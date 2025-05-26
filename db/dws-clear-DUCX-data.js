62491555ab0fe5aaec173bc42b4dd6ba1adb6335
Tavern_Master <dimasamusch@yandex.ru>
2025-05-26 15:40:05 +0300

    chore: add db migration scripts

---
+++ db/dws-clear-DUCX-data.js
@@ -0,0 +1,33 @@
+const db = db.getSiblingDB('dws');
+
+const walletIds = db.wallets
+  .find({ coin: 'ducx' }, { id: 1 })
+  .toArray()
+  .map(doc => doc.id);
+const txsDeleted = db.txs.deleteMany({
+  $or: [{ coin: 'ducx' }, { chain: 'ducx' }, { walletId: { $in: walletIds } }]
+});
+print(`Txs deleted: ${txsDeleted.deletedCount}`);
+const cacheDeleted = db.cache.deleteMany({
+  $or: [
+    { walletId: { $in: walletIds } },
+    {
+      $and: [
+        {
+          $or: walletIds.map(id => ({
+            walletId: { $regex: `^${id}` }
+          }))
+        },
+        { type: { $in: ['historyCacheStatusV8', 'historyCacheV8', 'historyStream'] } }
+      ]
+    }
+  ]
+});
+print(`Cache deleted: ${cacheDeleted.deletedCount}`);
+const addressesUnregistered = db.addresses.updateMany(
+  { walletId: { $in: walletIds } },
+  { $set: { beRegistered: null } }
+);
+print(`Addresses unregistered: ${addressesUnregistered.modifiedCount}`);
+const walletsUnregistered = db.wallets.updateMany({ id: { $in: walletIds } }, { $set: { beRegistered: null } });
+print(`Wallets unregistered: ${walletsUnregistered.modifiedCount}`);
